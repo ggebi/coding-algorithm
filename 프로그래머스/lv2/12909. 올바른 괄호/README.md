e220a3ed1698b69fcef2658c3ceb0903fd781089
HyungWoo Ju <41698939+jhwmon@users.noreply.github.com>
2022-12-27 01:23:41 -0800

    [level 2] Title: 올바른 괄호, Time: 5.72 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/README.md
+++ 프로그래머스/lv2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 39.9 MB, 시간: 9.05 ms
+메모리: 38.3 MB, 시간: 5.72 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,13 +1,8 @@
 function solution(s){
     let answer = true;
-    let sArr = Array.from(s);
     let bufferNum = 0;
     
-    if (sArr[0] === ')' || sArr[sArr.length - 1] === '(') {
-        return false;
-    }
-    
-    for (i of sArr) {
+    for (i of s) {
         if (i === '(') bufferNum++;
         else bufferNum--;
         
