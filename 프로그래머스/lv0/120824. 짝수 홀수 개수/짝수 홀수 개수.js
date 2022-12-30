da6c6849c3863b72530e53b31ddecb02b37f3998
HyungWoo Ju <41698939+jhwmon@users.noreply.github.com>
2022-12-29 16:47:55 -0800

    [level 0] Title: 짝수 홀수 개수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    let answer = [0, 0];
+    
+    for (i of num_list) {
+        if (i % 2 == 0) {
+            answer[0] += 1;
+        } else {
+            answer[1] += 1;
+        }
+    }
+    
+    return answer;
+}
